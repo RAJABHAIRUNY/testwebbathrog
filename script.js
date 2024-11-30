3a54345f02afc0c30b91719dcc26142dbb82d8dd
Raja Bhai Runy <rajabhairuny19@gmail.com>
2024-11-30 17:18:57 +0545

    Update script.js
     media for nav is corrected
    
    Signed-off-by: Raja Bhai Runy <rajabhairuny19@gmail.com>

--- script.js
+++ script.js
@@ -65,11 +65,7 @@ setInterval(function () {
 
 
 
-var navLinks = document.getElementById("navLinks")
-function showmenu()
-{navLinks.style.right = "0";}
-function hidemenu()
-{navLinks.style.right = "-200px";}
+
 
 
 
